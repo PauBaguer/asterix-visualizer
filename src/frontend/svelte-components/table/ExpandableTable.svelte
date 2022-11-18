3d4938f4e12bec41f11cdb162fe3a078346d2355
Pau Baguer <pauinki@gmail.com>
2022-11-18 22:38:24 +0100

    arrow

--- src/frontend/svelte-components/table/ExpandableTable.svelte
+++ src/frontend/svelte-components/table/ExpandableTable.svelte
@@ -27,6 +27,7 @@
   });
 
   let allChildComponents = {};
+  let allChildComponentsKeys = Object.keys(allChildComponents);
 
   function trClick(id) {
     let tr = document.getElementById(`tr-${id}`);
@@ -37,6 +38,7 @@
       tr.classList.remove("shown");
       allChildComponents[id].$destroy();
       delete allChildComponents[id];
+      allChildComponentsKeys = Object.keys(allChildComponents);
     } else {
       // Open this row
       tr.classList.add("shown");
@@ -44,6 +46,7 @@
       let nexttr = arr[arr.indexOf(tr) + 1];
       let child = new GenericProps({ target: tbody, anchor: nexttr });
       allChildComponents[id] = child;
+      allChildComponentsKeys = Object.keys(allChildComponents);
     }
   }
 
@@ -95,157 +98,24 @@
             {#each messages as msg, i}
               <tr on:click="{() => trClick(msg.id)}" id="tr-{msg.id}">
                 <td
-                  ><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button
-                  ></td
+                  ><button class="btn btn-default btn-xs">
+                    {#if allChildComponentsKeys.includes(msg.id.toString())}
+                      <i class="bi bi-arrow-down-short"></i>
+                    {:else}
+                      <i class="bi bi-arrow-right-short"></i>
+                    {/if}
+                  </button></td
                 >
                 <td>{msg.id}</td>
                 <td>{msg.class}</td>
                 <td>{msg.message_type}</td>
                 <td>{`${msg.data_source_identifier.SIC} ${msg.data_source_identifier.SAC}`}</td>
                 <td>{msg.time_of_day}</td>
-                <!-- {:else if msg.class === "Cat21"}
-                  <td>{msg.id}</td>
-                  <td>{msg.class}</td>
-                  <td>{msg.service_identification}</td>
-                  <td>{msg.data_source_identifier}</td>
-                  <td>tbd</td> -->
               </tr>
-
-              <!-- <tr>
-                <td colspan="12" class="hiddenRow">
-                  <div class="accordian-body collapse" id="{`demo${i}`}">
-                    {#each Object.keys(msg) as key, j}
-                      <a
-                        class="btn btn-primary"
-                        data-toggle="collapse"
-                        href="#multiCollapseExample1"
-                        role="button"
-                        aria-expanded="false"
-                        aria-controls="multiCollapseExample1">key</a
-                      >
-                      <div class="collapse multi-collapse" id="multiCollapseExample1">
-                        worked?
-                         {#if key === "target_report_description" && msg.class === "Cat10"}
-                            <TargetReportDescription
-                              i="{i}"
-                              j="{j}"
-                              targetReportDescription="{msg.target_report_description}"
-                            />
-                          {:else if key === "cartesian_coordinates" && msg.class === "Cat10"}
-                            <CartesianCoordinates i="{i}" j="{j}" cartesianCoordinates="{msg.cartesian_coordinates}" />
-                          {:else if key != "id" && key != "time_of_day" && key != "message_type" && key !== "class"}
-                            {#if typeof msg[key] == "object"}
-                              <GenericComponent i="{i}" j="{j}" genericObject="{msg[key]}" dataItemName="{key}" />
-                            {:else}
-                              <td></td>
-                              <td></td>
-                              <td>{key}</td>
-                              <td>{msg[key]}</td>
-                              <td></td>
-                              <td></td>
-                              <td></td>
-                              <td></td>
-                            {/if}
-                          {/if} 
-                      </div>
-                    {/each}
-
-                     <table class="table table-striped table-bordered table-sm" cellspacing="0">
-                      <thead>
-                        <tr class="info">
-                          <th></th>
-                          <th></th>
-                          <th>Item</th>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                        </tr>
-                      </thead>
-
-                      <tbody>
-                        {#each Object.keys(msg) as key, j}
-                          {#if key === "target_report_description" && msg.class === "Cat10"}
-                            <TargetReportDescription
-                              i="{i}"
-                              j="{j}"
-                              targetReportDescription="{msg.target_report_description}"
-                            />
-                          {:else if key === "cartesian_coordinates" && msg.class === "Cat10"}
-                            <CartesianCoordinates i="{i}" j="{j}" cartesianCoordinates="{msg.cartesian_coordinates}" />
-                          {:else if key != "id" && key != "time_of_day" && key != "message_type" && key !== "class"}
-                            {#if typeof msg[key] == "object"}
-                              <GenericComponent i="{i}" j="{j}" genericObject="{msg[key]}" dataItemName="{key}" />
-                            {:else}
-                              <td></td>
-                              <td></td>
-                              <td>{key}</td>
-                              <td>{msg[key]}</td>
-                              <td></td>
-                              <td></td>
-                              <td></td>
-                              <td></td>
-                            {/if}
-                          {/if}
-                        {/each}
-                      </tbody>
-                    </table> 
-                  </div>
-                </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr> -->
             {/each}
-            <!-- <tr data-toggle="collapse" data-target="#demo2" class="accordion-toggle">
-              <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td
-                >
-                <td>Silvio</td>
-                <td>Santos</td>
-                <td>São Paulo</td>
-                <td>SP</td>
-                <td> new</td>
-              </tr>
-              <tr>
-                <td colspan="6" class="hiddenRow"><div id="demo2" class="accordian-body collapse">Demo2</div></td>
-              </tr>
-              <tr data-toggle="collapse" data-target="#demo3" class="accordion-toggle">
-                <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td
-                  >
-                  <td>John</td>
-                  <td>Doe</td>
-                  <td>Dracena</td>
-                  <td>SP</td>
-                  <td> New</td>
-            </tr>
-            <tr>
-              <td colspan="6" class="hiddenRow"
-                ><div id="demo3" class="accordian-body collapse">Demo3 sadasdasdasdasdas</div></td
-              >
-            </tr> -->
           </tbody>
         </table>
       </div>
     </div>
   </div>
-  <!-- <div class="text-center">
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item" on:click="{() => handlePageClick(activePage - 1)}">
-          <a class="page-link" href="#a">Previous</a>
-        </li>
-        {#each displayedPageArray as page}
-          <li class="{activePage === page ? 'page-item active' : 'page-item'}" on:click="{() => handlePageClick(page)}">
-            <a class="page-link" href="#a">{page}</a>
-          </li>
-        {/each}
-        <li class="page-item" on:click="{() => handlePageClick(activePage + 1)}">
-          <a class="page-link" href="#a">Next</a>
-        </li>
-      </ul>
-    </nav>
-  </div> -->
 </div>
