383da1211774027bce8a92780497e647fc6af35c
Pau Baguer <pauinki@gmail.com>
2022-12-02 19:34:38 +0100

    mlat areas

--- src/electron/utils/mlatAreas.ts
+++ src/electron/utils/mlatAreas.ts
@@ -21,11 +21,11 @@ import { isPointInPolygon } from "geolib";
 // to
 
 const poly = [
-  { latitude: 2.073544428747862, longitude: 41.282322777493 },
-  { latitude: 2.1038044835174534, longitude: 41.292695924731504 },
-  { latitude: 2.104090847313795, longitude: 41.29222048305417 },
-  { latitude: 2.07382476073992, longitude: 41.28186917069003 },
-  { latitude: 2.073544428747862, longitude: 41.282322777493 },
+  { longitude: 2.073544428747862, latitude: 41.282322777493 },
+  { longitude: 2.1038044835174534, latitude: 41.292695924731504 },
+  { longitude: 2.104090847313795, latitude: 41.29222048305417 },
+  { longitude: 2.07382476073992, latitude: 41.28186917069003 },
+  { longitude: 2.073544428747862, latitude: 41.282322777493 },
 ];
 
 isPointInPolygon({ latitude: 42, longitude: 2 }, poly);
